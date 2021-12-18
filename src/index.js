895d97165669383f115eb79f5d77782172d7eeb1
Muhammad Bilal <mbilal@sirma.com>
2021-12-18 13:16:25 +0500

    webpack configured successfully

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-/* //css
-import style from '/src/css/style.css';
-import bootstrapcss from '/src/css/bootstrap.min.css';
+//css
+import './css/style.css'
+import './css/bootstrap.min.css'
 
 //images
-import favicon from '/src/images/esgifavicon.ico';
-import background from '/src/images/esgibg.png';
-import green from '/src/images/green.jpg';
+import favicon from './images/esgifavicon.ico'
+import background from './images/esgibg.png'
+import green from './images/green.jpg'
 
 //js
-import isotope from '/src/js/isotope.js';
-import scroll from '/src/js/scrollspy.min.js';
-import bootstrapjs from '/src/js/bootstrap.bundle.min.js';
-import jquery from '/src/js/jquery.min.js';
-import jeasing from '/src/js/jquery.easing.min.js';
-import jpop from '/src/js/jquery.magnific-popup.min.js';
-import carousel from "/src/js/owl.carousel.min.js";
-import app from "/src/js/app.js"; */
+import isotope from './js/isotope.js'
+import scroll from './js/scrollspy.min.js'
+import bootstrapjs from './js/bootstrap.bundle.min.js'
+import jquery from './js/jquery.min.js'
+import jeasing from './js/jquery.easing.min.js'
+import jpop from './js/jquery.magnific-popup.min.js'
+import carousel from './js/owl.carousel.min.js'
+import app from './js/app.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-var webpack = require('webpack');
+var webpack = require('webpack')
 
 //global variables
 //const rootPath = path.resolve(__dirname);
@@ -9,62 +9,60 @@ var webpack = require('webpack');
 
 // webpack
 module.exports = {
-    //mode
-    mode: 'development',
-    //entry
-    entry: '/src/css/style.css',
-    //performance & size
-    performance: {
-        hints: false,
+  //mode
+  mode: 'development',
+  //entry
+  entry: '/src/index.js',
+  //performance & size
+  performance: {
+    hints: false,
+  },
+  //output
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    publicPath: 'auto',
+  },
+  //webpack serve
+  devtool: false, //'inline-source-map',
+  stats: 'none',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+  //loaders
+  module: {
+    rules: [
+      //css
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      //all images
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+        },
+        dependency: { not: ['url'] },
+        type: 'asset/resource',
       },
-    //output
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        clean: true,
-        publicPath: 'atuo',
-    },
-    //webpack serve
-    devtool: false, //'inline-source-map',
-    stats: 'none',
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 8080,
-        open: true,
-        hot: true,
-    },
-    //loaders
-    module:{
-        rules: [
-            //css
-            {   test: /\.css$/i, 
-                use: ['style-loader', 'css-loader'] 
-            },
-            //all images
-            {
-                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-                use: {
-                  loader: 'file-loader',
-                },
-                dependency: { not: ['url'] },
-                type: 'asset/resource'
-              },
-            //js for babel
-            {   test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            //html
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                loader: 'html-loader'
-            }
-        ]
-    },
-    //plugins
-    plugins: [
-        new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
-        new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' })
+      //js for babel
+      { test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader' },
+      //html
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'html-loader',
+      },
     ],
-};
+  },
+  //plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
+  ],
+}
